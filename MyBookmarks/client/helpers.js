09586ccab32530eadab62eb81132e45118a84b5e
Clement Pigeon <clementpigeon@gmail.com>
2014-05-08 16:30:20 +0200

    user can bookmark current page

--- MyBookmarks/client/helpers.js
+++ MyBookmarks/client/helpers.js
@@ -6,7 +6,15 @@ Template.main.bookmarks = function () {
 
 Template.main.events({
   'click input#newBookmark': function () {
-      var url = window.location.hash.slice(1);
-      console.log(url);
+    var fragment = window.location.hash.slice(1);
+    var url = decodeURIComponent(fragment.split('&')[0]);
+    var title = decodeURIComponent(fragment.split('&')[1]);
+
+    Bookmarks.insert({
+      userId: Meteor.userId(),
+      created: new Date().getTime(),
+      title: title,
+      url: url
+    });
   }
 });
