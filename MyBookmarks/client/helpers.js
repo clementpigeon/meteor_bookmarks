6b064977bf40510e69fa01241373419863a8e680
Clement Pigeon <clementpigeon@gmail.com>
2014-05-08 15:05:25 +0200

    create meteor app with accounts

---
+++ MyBookmarks/client/helpers.js
@@ -0,0 +1,13 @@
+Template.hello.greeting = function () {
+  return "Welcome to MyBookmarks.";
+};
+
+Template.hello.events({
+  'click input': function () {
+    // template data, if any, is available in 'this'
+    if (typeof console !== 'undefined')
+      console.log("You pressed the button");
+  }
+});
+
+
