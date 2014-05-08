d6b3099173b751fcb43c7a0892e955ff6dcb5853
Clement Pigeon <clementpigeon@gmail.com>
2014-05-08 16:40:47 +0200

    remove insecure package, add permissions on bookmarks

--- MyBookmarks/collections/bookmarks.js
+++ MyBookmarks/collections/bookmarks.js
@@ -1,1 +1,11 @@
 Bookmarks = new Meteor.Collection('bookmarks');
+
+ownsBookmark = function(userId, bookmark){
+  return bookmark && bookmark.userId === userId;
+}
+
+Bookmarks.allow({
+  insert: ownsBookmark,
+  update: ownsBookmark,
+  remove: ownsBookmark
+});
