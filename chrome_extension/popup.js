befaec1fab952c687400b746d20d9fcd5e5c3220
Clement Pigeon <clementpigeon@gmail.com>
2014-05-08 22:11:30 +0200

    switch to deployed url, add icon

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -2,6 +2,6 @@ chrome.tabs.getSelected(null, function(tab) {
     var currentUrl = encodeURIComponent(tab.url);
     var title = encodeURIComponent(tab.title);
     var fragment = currentUrl + "&" + title;
-    var iframeUrl = "http://localhost:3000/#" + fragment
+    var iframeUrl = "http://meteor_bookmarks.meteor.com/#" + fragment
     document.getElementById('meteorIframe').src = iframeUrl;
 });
