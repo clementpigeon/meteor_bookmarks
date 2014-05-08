73334eb2f7c5184e695db2158ad88d403a2f5323
Clement Pigeon <clementpigeon@gmail.com>
2014-05-08 18:08:55 +0200

    user can add note after creating bookmark

--- MyBookmarks/client/helpers.js
+++ MyBookmarks/client/helpers.js
@@ -1,7 +1,12 @@
 Meteor.subscribe('bookmarks', Meteor.userId());
 
-Template.bookmarkList.bookmarks = function () {
-  return Bookmarks.find({});
+Template.main.rendered = function(){
+  Session.set('newBookmarkId', null);
+}
+
+Template.main.newBookmark = function () {
+  var newBookmarkId = Session.get('newBookmarkId');
+  return newBookmarkId && Bookmarks.findOne(newBookmarkId);
 };
 
 Template.main.events({
@@ -10,11 +15,41 @@ Template.main.events({
     var url = decodeURIComponent(fragment.split('&')[0]);
     var title = decodeURIComponent(fragment.split('&')[1]);
 
-    Bookmarks.insert({
+    var newBookmarkId = Bookmarks.insert({
       userId: Meteor.userId(),
       created: new Date().getTime(),
       title: title,
       url: url
     });
+
+    Session.set('newBookmarkId', newBookmarkId);
+  }
+});
+
+Template.bookmarkList.bookmarks = function () {
+  return Bookmarks.find({}, {sort: {'created': -1}});
+};
+
+Template.addNote.events({
+  'click input#dismiss': function (e) {
+    e.preventDefault();
+    Session.set('newBookmarkId', null);
+  },
+
+  'submit form': function (e) {
+    e.preventDefault();
+    var title = $(e.target).find('[name=title]').val();
+    var note = $(e.target).find('[name=note]').val();
+
+    Bookmarks.update(
+      {"_id": Session.get('newBookmarkId')},
+      {
+        $set : {
+          "title": title,
+          "note": note
+        }
+      }
+    )
+    Session.set('newBookmarkId', null);
   }
 });
