47f7a97af522c9f04338ad7059650e8786bd427b
Clement Pigeon <clementpigeon@gmail.com>
2014-05-08 16:26:03 +0200

    chrome extension passes both page url and title to iframe

--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,4 +1,7 @@
 chrome.tabs.getSelected(null, function(tab) {
-    var currentUrl = tab.url;
-    document.getElementById('meteorIframe').src = "http://localhost:3000/#" + currentUrl;
+    var currentUrl = encodeURIComponent(tab.url);
+    var title = encodeURIComponent(tab.title);
+    var fragment = currentUrl + "&" + title;
+    var iframeUrl = "http://localhost:3000/#" + fragment
+    document.getElementById('meteorIframe').src = iframeUrl;
 });
